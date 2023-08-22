f767cd777fb0de830f3b8cd00980d1808c4d3667
Lucas de Souza <lucasdesouza@Air-de-Lucas.lan>
2023-08-22 19:55:11 -0300

    first commit

---
+++ app-root/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// import Header from "components/Header";
+
+const App = () => (
+  <>
+    <h1>Root app</h1>
+  </>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
