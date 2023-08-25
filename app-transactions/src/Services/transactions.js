7b28a933b604f3fc933a7c1a9a3bde1472e59be2
Lucas de Souza <lucasdesouza@Air-de-Lucas.lan>
2023-08-24 21:05:56 -0300

    feat: add paginated data transactions and by dynamic period

--- app-transactions/src/Services/transactions.js
+++ app-transactions/src/Services/transactions.js
@@ -5,7 +5,6 @@ export default class TransactionsService {
     return response;
   }
 
-  // async findByPeriod({year: 2023, month = 7, offset= 0, limit = 50, backendToken}) {
   async findByPeriod({ year = 2023, month = 7, offset = 0, limit = 50, backendToken }) {
     const response = await api.get(
       `/transactions/user/view/period?year=${year}&month=${month}&limit=${limit}&offset=${offset}`,
