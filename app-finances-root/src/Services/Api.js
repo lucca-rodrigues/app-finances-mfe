ed91bd2c04e1aceecaff15a4e90ba16466fdb890
Lucas de Souza <lucasdesouza@Air-de-Lucas.lan>
2024-01-05 12:42:00 -0300

    update base url

--- app-finances-root/src/Services/Api.js
+++ app-finances-root/src/Services/Api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 // const apiUrl = "http://localhost:3333";
-const apiUrl = "https://api-finances.innova-systems.tec.br";
+// const apiUrl = "https://api-finances.innova-systems.tec.br";
+const apiUrl = "http://192.168.0.229:3333";
 
 const api = axios.create({
   baseURL: apiUrl,
--- app-transactions/src/Services/api.js
+++ app-transactions/src/Services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api-finances.innova-systems.tec.br",
+  // baseURL: "https://api-finances.innova-systems.tec.br",
+  baseURL: "http://192.168.0.229:3333",
 });
 
 export default api;
