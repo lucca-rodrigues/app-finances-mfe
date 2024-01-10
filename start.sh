a0ac7f672fd55b64ae42d2872b11ac85e279c117
Lucas de Souza <lucasdesouza@Air-de-Lucas.lan>
2024-01-10 15:37:59 -0300

    update script

--- start.sh
+++ start.sh
@@ -3,31 +3,34 @@ echo "AUTOMATIZATION"
 echo "============================>"
 
 
-cd app-finances-root
-npm i
-pm2 start ecosystem.config.js
-
 echo "============================"
 echo "Finances Root Started"
 echo "============================"
-
-
-cd ../app-my-account
+cd app-finances-root
 npm i
+npm run build
 pm2 start ecosystem.config.js
+
+
 
 echo "============================"
 echo "My Account Started"
 echo "============================"
-
-
-cd ../app-transactions
+cd ../app-my-account
 npm i
+npm run build
 pm2 start ecosystem.config.js
+
+
 
 echo "============================"
 echo "Transactions Started"
 echo "============================"
+cd ../app-transactions
+npm i
+npm run build
+pm2 start ecosystem.config.js
+
 
 
 echo "============================>"
